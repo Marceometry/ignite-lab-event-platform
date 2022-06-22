5c4225f80ecfc8e28610145c6dffbab146122138
Marceometry <marceometry@gmail.com>
2022-06-22 10:26:41 -0300

    feat: finish lessons sidebar

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,24 +1,54 @@
-import { CheckCircle } from 'phosphor-react'
+import { isPast, format } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+import { CheckCircle, Lock } from 'phosphor-react'
 
-export const Lesson = () => {
+type LessonProps = {
+  title: string
+  slug: string
+  availableAt: Date
+  type: 'live' | 'class'
+}
+
+export const Lesson = ({ title, availableAt, type }: LessonProps) => {
+  const isLessonAvailable = isPast(availableAt)
+
+  const formattedDate = format(availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {
+    locale: ptBR,
+  })
+
+  const formattedDateFirstLetter = formattedDate.charAt(0)
+
+  const formattedDateWithFirstLetterUppercase = formattedDate.replace(
+    formattedDateFirstLetter,
+    formattedDateFirstLetter.toUpperCase()
+  )
+
   return (
     <a href='#'>
-      <span className='text-gray-300'>Terça - 22 de junho - 19h00</span>
+      <span className='text-gray-300'>
+        {formattedDateWithFirstLetterUppercase}
+      </span>
 
       <div className='rounded border border-gray-500 p-4 mt-2'>
         <header className='flex items-center justify-between'>
-          <span className='text-sm text-blue-500 font-medium flex'>
-            <CheckCircle size={20} />
-            Conteúdo liberado
-          </span>
+          {isLessonAvailable ? (
+            <span className='text-sm text-blue-500 font-medium flex items-center gap-2'>
+              <CheckCircle size={20} />
+              Conteúdo liberado
+            </span>
+          ) : (
+            <span className='text-sm text-orange-500 font-medium flex items-center gap-2'>
+              <Lock size={20} />
+              Em breve
+            </span>
+          )}
+
           <span className='text-xs rounded px-2 py-[0.125rem] text-white border border-green-300 font-bold'>
-            AO VIVO
+            {type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
           </span>
         </header>
 
-        <strong className='text-gray-200 mt-5 block'>
-          Abertura do evento Ignite Lab
-        </strong>
+        <strong className='text-gray-200 mt-5 block'>{title}</strong>
       </div>
     </a>
   )
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,31 @@
+import { gql, useQuery } from '@apollo/client'
 import { Lesson } from './Lesson'
 
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+      id
+      lessonType
+      slug
+      title
+      availableAt
+    }
+  }
+`
+
+type GetLessonsQueryResponse = {
+  lessons: Array<{
+    id: string
+    title: string
+    slug: string
+    availableAt: string
+    lessonType: 'live' | 'class'
+  }>
+}
+
 export const Sidebar = () => {
+  const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY)
+
   return (
     <aside className='w-[348px] bg-gray-700 p-6 border-l border-gray-600'>
       <span className='font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block'>
@@ -8,12 +33,15 @@ export const Sidebar = () => {
       </span>
 
       <div className='flex flex-col gap-8'>
-        <Lesson />
-        <Lesson />
-        <Lesson />
-        <Lesson />
-        <Lesson />
-        <Lesson />
+        {data?.lessons.map((lesson) => (
+          <Lesson
+            key={lesson.id}
+            slug={lesson.slug}
+            title={lesson.title}
+            type={lesson.lessonType}
+            availableAt={new Date(lesson.availableAt)}
+          />
+        ))}
       </div>
     </aside>
   )
